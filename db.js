5a80224c310df3d54c8c3a8d70cfe91f55ebfcf5
FISmart1 <yusufftibazma@gmail.com>
2025-11-16 00:02:35 +0700

    ubah db

--- db.js
+++ db.js
@@ -2,9 +2,9 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "127.0.0.1",
+  user: "adminppdb",
+  password: "spmb2026",
   database: "ppdb"
 });
 
